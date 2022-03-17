fb593601be76db40615eb8c15d912ddbf4c05b06
user <user@swjang>
2022-03-17 18:04:12 +0900

    header 수정 중

--- src/main/java/com/oms/config/WebConfig.java
+++ src/main/java/com/oms/config/WebConfig.java
@@ -8,17 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	/**
+	 * 인터셉터 Bean 주입
+	 * @return
+	 */
 	@Bean
 	public CommonInterceptor commonInterceptor() {
 		return new CommonInterceptor();
 	}
 	
+	/**
+	 * 인터셉터에서 제외할 항목 설정
+	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(commonInterceptor())
 				.order(1)
 				.addPathPatterns("/**")
-				.excludePathPatterns("/css/**", "/vendor/**", "/js/**", "/fonts/**", "/img/**", "/scss/**");
+				.excludePathPatterns("/css/**", "/vendor/**", "/js/**", "/fonts/**", "/img/**", "/scss/**", "/svg/**");
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
 }
