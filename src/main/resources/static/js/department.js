18f340533fe61fe5f199446163765641876923a1
user <user@swjang>
2022-05-12 12:09:00 +0900

    프론트엔드 고도화 진행 예정

--- src/main/resources/static/js/department.js
+++ src/main/resources/static/js/department.js
@@ -2,8 +2,32 @@
 $(function() {
 	// 부서 테이블 설정
 	// =======================================================
-	HSCore.components.HSDatatables.init($('#departmentTable'), {
+	var departmentTable = $('#departmentTable').DataTable({
+		// 반응형 테이블 설정
+		responsive: true,
 		dom: 'frtip',
+		// 개별 컬럼 설정 
+		columnDefs: [
+			{ targets: 0, data: 0, checkboxes: { selectRow: true }, orderable: false },
+			{ targets: 3, orderable: false }
+		],
+		//  용어 설정
+		language: {
+			zeroRecords: `<div class="text-center p-4">
+              <img class="mb-3" src="/svg/illustrations/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="default">
+              <img class="mb-3" src="/svg/illustrations-light/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="dark">
+            <p class="mb-0">부서가 없습니다</p>
+            </div>`,
+			select: {
+				rows: {
+					_: '%d개 선택됨',
+					0: ''
+				}
+			}
+		},
+		search: {
+			caseInsensitive: false
+		},
 		select: {
 			style: 'multi',
 			selector: 'td:first-child input[type="checkbox"]',
@@ -13,49 +37,66 @@ $(function() {
 				counterInfo: '#departmentCounterInfo'
 			}
 		},
-		columnDefs: [
-			{
-				targets: [0],
-				orderable: false
-			}
-		],
-		order: [],
-		info: {
-			totalQty: '#departmentWithPaginationInfoTotalQty'
-		},
 		search: '#departmentSearch',
-		entries: '#departmentEntries',
-		pageLength: 10,
-		isResponsive: true,
-		isShowPaging: false,
-		pagination: 'departmentPagination',
-		language: {
-			zeroRecords: `<div class="text-center p-4">
-              <img class="mb-3" src="/svg/illustrations/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="default">
-              <img class="mb-3" src="/svg/illustrations-light/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="dark">
-            <p class="mb-0">부서가 없습니다</p>
-            </div>`
-		},
-		// 테이블 fnDrawCallback이 발생하면 등록 버튼을 다시 보이게 하기
-		'fnDrawCallback': function(oSettings) {
-			$('#createDepartmentRow').removeClass('d-none');
-		}
+					isShowPaging: true,
+			pagination: 'departmentPagination',
+		order: [1, 'asc']
 	});
+	
+	/*	HSCore.components.HSDatatables.init($('#departmentTable'), {
+			dom: 'frtip',
+			select: {
+				style: 'multi',
+				selector: 'td:first-child input[type="checkbox"]',
+				classMap: {
+					checkAll: '#departmentCheckAll',
+					counter: '#departmentCounter',
+					counterInfo: '#departmentCounterInfo'
+				}
+			},
+			columnDefs: [
+				{
+					targets: [0],
+					orderable: false
+				}
+			],
+			order: [],
+			info: {
+				totalQty: '#departmentWithPaginationInfoTotalQty'
+			},
+			search: '#departmentSearch',
+			entries: '#departmentEntries',
+			pageLength: 10,
+			isResponsive: true,
+			isShowPaging: true,
+			pagination: 'departmentPagination',
+			language: {
+				zeroRecords: `<div class="text-center p-4">
+				  <img class="mb-3" src="/svg/illustrations/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="default">
+				  <img class="mb-3" src="/svg/illustrations-light/oc-error.svg" alt="Image Description" style="width: 10rem;" data-hs-theme-appearance="dark">
+				<p class="mb-0">부서가 없습니다</p>
+				</div>`
+			},
+			// 테이블 fnDrawCallback이 발생하면 등록 버튼을 다시 보이게 하기
+			'fnDrawCallback': function(oSettings) {
+				$('#createDepartmentRow').removeClass('d-none');
+			}
+		});*/
 	// 부서 사용유무에 따라 체크박스 값 표시 변경
 	$('td[name=departmentUseYn]').each(function() {
 		if ($(this).children(':first').text() == 'Y') {
 			$(this).children(':eq(1)').children(':first').prop('checked', true);
 		}
 	});
-	
+
 	// 등록시간, 수정시간 포맷 변경
-/*	$('td[name=departmentRegistTime]').each(function() {
-		$(this).text(moment($(this).text()).format('YYYY-MM-DD'));
-	});
-	$('td[name=departmentUpdateTime]').each(function() {
-		$(this).text(moment($(this).text()).format('YYYY-MM-DD'));
-	});*/
-	
+	/*	$('td[name=departmentRegistTime]').each(function() {
+			$(this).text(moment($(this).text()).format('YYYY-MM-DD'));
+		});
+		$('td[name=departmentUpdateTime]').each(function() {
+			$(this).text(moment($(this).text()).format('YYYY-MM-DD'));
+		});*/
+
 });
 
 // ======================= Functions =======================//
@@ -65,15 +106,15 @@ $(function() {
 function activateDepartmentSaveButton(id) {
 	// 사용여부 값 세팅
 	var departmentUse = 'N';
-	if ($('#departmentUseYn'+id).is(':checked') == true) {
+	if ($('#departmentUseYn' + id).is(':checked') == true) {
 		departmentUse = 'Y';
 	}
 	// 부서명 또는 사용여부에 변화가 생기면 부서 저장 버튼이 활성화
-	if ($('#departmentName'+id).val() != $('#departmentName'+id).parent().children(':first').text() ||
-			$('#departmentUseYn'+id).parent().parent().children(':first').text() != departmentUse) {
-		$('#departmentSaveButton'+id).attr('disabled', false);
+	if ($('#departmentName' + id).val() != $('#departmentName' + id).parent().children(':first').text() ||
+		$('#departmentUseYn' + id).parent().parent().children(':first').text() != departmentUse) {
+		$('#departmentSaveButton' + id).attr('disabled', false);
 	} else {
-		$('#departmentSaveButton'+id).attr('disabled', true);
+		$('#departmentSaveButton' + id).attr('disabled', true);
 	}
 }
 
@@ -86,20 +127,20 @@ function createDepartmentRow() {
 	$('[name=departmentId]').each(function() {
 		ids.push($(this).val());
 	});
-	var newId = Math.max(...ids)+1;
+	var newId = Math.max(...ids) + 1;
 	$('#departmentList').append(`<tr class="bg-success">
 																<td class="table-column-pe-0"></td>
 																<td class="table-column-ps-0">
-																	<input type="text" class="ms-3 ps-2 border bg-light text-dark w-auto" id="departmentName`+newId+`" placeholder="부서명을 입력하세요" onChange="departmentNameCheck(`+newId+`, this.value)">
+																	<input type="text" class="ms-3 ps-2 border bg-light text-dark w-auto" id="departmentName`+ newId + `" placeholder="부서명을 입력하세요" onChange="departmentNameCheck(` + newId + `, this.value)">
 																</td>
 																<td name="departmentUseYn">
 																	<div class="form-check form-switch">
-																		<input class="form-check-input" type="checkbox" id="departmentUseYn`+newId+`">
-																		<label class="form-check-label" for="departmentUseYn`+newId+`"></label>
+																		<input class="form-check-input" type="checkbox" id="departmentUseYn`+ newId + `">
+																		<label class="form-check-label" for="departmentUseYn`+ newId + `"></label>
 																	</div>
 																</td>
 																<td>
-																	<a class="btn btn-primary btn-sm" href="javascript:;" onClick="createDepartment(`+newId+`)">
+																	<a class="btn btn-primary btn-sm" href="javascript:;" onClick="createDepartment(`+ newId + `)">
 																		<i class="bi-plus-lg"></i> 등록
 																	</a>
 																	<a class="btn btn-danger btn-sm" href="javascript:;" onClick="deleteDepartmentRow(this)">
@@ -120,33 +161,33 @@ function deleteDepartmentRow(row) {
 /*******************
 * 부서명 검증(등록) *
 ********************/
-function departmentNameCheck(id, name) {	
+function departmentNameCheck(id, name) {
 	var pattern = /\s/g;
 	if (name == '' || name == undefined || name == null) {
-		$('#departmentName'+id).focus();
-		$('#departmentName'+id).select();
+		$('#departmentName' + id).focus();
+		$('#departmentName' + id).select();
 		return;
 	}
 	if (name.match(pattern)) {
 		alert('부서명에 공백이 존재할 수 없습니다');
-		$('#departmentName'+id).focus();
-		$('#departmentName'+id).select();
+		$('#departmentName' + id).focus();
+		$('#departmentName' + id).select();
 		return;
 	}
 	// 중복되는 부서가 있는지 확인
 	$.ajax({
 		contentType: 'application/json; charset=utf-8',
-		url: '/api/department',			
+		url: '/api/department',
 		type: 'GET',
 		data: {
 			name: name
 		},
 		cache: false,
 		success: function(result) {
-			if (result.departmentList.length > 0) {					
+			if (result.departmentList.length > 0) {
 				alert('중복되는 부서명이 존재합니다');
-				$('#departmentName'+id).focus();
-				$('#departmentName'+id).select();
+				$('#departmentName' + id).focus();
+				$('#departmentName' + id).select();
 				return;
 			}
 		},
@@ -161,17 +202,17 @@ function departmentNameCheck(id, name) {
 /*******************
 * 부서명 검증(수정) *
 ********************/
-function updateDepartmentNameCheck(id, name) {	
+function updateDepartmentNameCheck(id, name) {
 	var pattern = /\s/g;
 	if (name == '' || name == undefined || name == null) {
-		$('#departmentName'+id).focus();
-		$('#departmentName'+id).select();
+		$('#departmentName' + id).focus();
+		$('#departmentName' + id).select();
 		return;
 	}
 	if (name.match(pattern)) {
 		alert('부서명에 공백이 존재할 수 없습니다');
-		$('#departmentName'+id).focus();
-		$('#departmentName'+id).select();
+		$('#departmentName' + id).focus();
+		$('#departmentName' + id).select();
 		return;
 	}
 	return name;
@@ -181,19 +222,19 @@ function updateDepartmentNameCheck(id, name) {
 * 부서 등록 *
 *************/
 function createDepartment(id) {
-	let name = departmentNameCheck(id, $('#departmentName'+id).val());
+	let name = departmentNameCheck(id, $('#departmentName' + id).val());
 	if (!name) {
 		return;
 	}
 	let useYn = 'N';
-	if ($('#departmentUseYn'+id).is(':checked') == true) {
-		useYn = 'Y';		
+	if ($('#departmentUseYn' + id).is(':checked') == true) {
+		useYn = 'Y';
 	}
 	var createData = JSON.stringify({
 		name: name,
 		useYn: useYn
 	});
-	
+
 	if (confirm('부서를 등록하시겠습니까?')) {
 		$.ajax({
 			contentType: 'application/json; charset=utf-8',
@@ -217,21 +258,21 @@ function createDepartment(id) {
 /************
 * 부서 수정 *
 *************/
-function updateDepartment(id) {	
-	let name = updateDepartmentNameCheck(id, $('#departmentName'+id).val());
+function updateDepartment(id) {
+	let name = updateDepartmentNameCheck(id, $('#departmentName' + id).val());
 	if (!name) {
 		return;
 	}
 	var useYn = 'N';
-	if ($('#departmentUseYn'+id).is(':checked') == true) {
+	if ($('#departmentUseYn' + id).is(':checked') == true) {
 		useYn = 'Y';
-	}	
+	}
 	var updateDate = JSON.stringify({
 		id: id,
 		name: name,
 		useYn: useYn
 	});
-	
+
 	if (confirm('변경한 부서 정보를 저장하시겠습니까?')) {
 		$.ajax({
 			contentType: 'application/json; charset=utf-8',
@@ -258,17 +299,18 @@ function deleteDepartment(param) {
 	// 체크한 부서 삭제인 경우 (param이 undefined)
 	if (param == undefined) {
 		var param = [];
-		$('input:checkbox[name=departmentId]').each(function() {
+		/*$('input:checkbox[name=departmentId]').each(function() {
 			if ($(this).is(':checked') == true) {
 				param.push($(this).val());
 			}
-		});		
+		});*/
+		param.push($('#departmentTable').columns().checkboxes.selected()[0]);
 	}
 	// 부서 삭제
-	if (confirm('선택한 부서를 삭제하시겠습니까?')) {
+	if (confirm('선택한 부서를 삭제하시겠습니까?' + param)) {
 		$.ajax({
 			contentType: 'application/json; charset=utf-8',
-			url: '/api/department/'+param,
+			url: '/api/department/' + param,
 			type: 'DELETE',
 			cache: false,
 			success: function(result) {
